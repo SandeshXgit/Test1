4c66d449f44f33d2500d8421bc73fbf2d57fd28f
SandeshXgit <jc9241254@gmail.com>
2025-06-18 22:27:46 +0545

    loop added in main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("hello");
+        for(int i =0; i<=5; i++){
+            System.out.println("hello");
+        }
         }
     }
 
